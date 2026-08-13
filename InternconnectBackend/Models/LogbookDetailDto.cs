9f5cdde64c3b1c70b7a627613e7e8310110f4308
Weda Wesnawa <komangweda360@gmail.com>
2026-08-13 14:16:56 +0800

    Add project files.

---
+++ InternconnectBackend/Models/LogbookDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternconnectBackend.Models
+{
+    public class LogbookDetailDto
+    {
+        public DateTime Date { get; set; }
+        public string Deskripsi { get; set; }
+        public string? Kendala { get; set; }
+        public string StatusAttend { get; set; }
+        public TimeSpan TimeStart { get; set; }
+        public TimeSpan TimeEnd { get; set; }
+        public string? Status { get; set; }
+    }
+}
---
+++ InternconnectBackend/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace InternconnectBackend.Models
+{
+    public class Login
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
